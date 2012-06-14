8f2012b7e7f9093a649dd9f0f9529c0b3cc3c295
Patrick Reilly <preilly@wikimedia.org>
2012-06-14 12:34:52 -0700

    add logging support
    Change-Id: I6db25625a3d751ee9dc8eec2ab50dfec687075f9

--- DeviceMapLogCapture.hooks.php
+++ DeviceMapLogCapture.hooks.php
@@ -22,20 +22,37 @@ class DeviceMapLogCaptureHooks {
 	}
 
 	public static function recordDevice( $eventId, $token, $site, $deviceMap, $countryCode, $userAgent ) {
+		global $wgDeviceMapDatabase, $wgDeviceMapLog;
+		$retval = true;
+		if ( $wgDeviceMapDatabase ) {
+			$dbw = wfGetDB( DB_MASTER );
+			$dbw->begin();
+			$data = array(
+				'action_time' => $dbw->timestamp(),
+				'session_id' => (string) $token,
+				'site' => (string) $site,
+				'event_id' => (int) $eventId,
+				'dmap' => (string) $deviceMap,
+				'country_code' => (string) $countryCode,
+				'user_agent' => (string) $userAgent,
+			);
+			$db_status = $dbw->insert( 'device_map_log_capture', $data, __METHOD__ );
+			$dbw->commit();
+		}
 
-	$retval = true;
-	$dbw = wfGetDB( DB_MASTER );
-	$dbw->begin();
-	$data = array(
-		'action_time' => $dbw->timestamp(),
-		'session_id' => (string) $token,
-		'site' => (string) $site,
-		'event_id' => (int) $eventId,
-		'dmap' => (string) $deviceMap,
-		'country_code' => (string) $countryCode,
-		'user_agent' => (string) $userAgent,
-	);
-	$db_status = $dbw->insert( 'device_map_log_capture', $data, __METHOD__ );
-	$dbw->commit();
+		if ( $wgDeviceMapLog ) {
+			$msg = implode( "\t", array(
+				wfTimestampNow(),
+				// Replace tabs with spaces in all strings
+				str_replace( "\t", ' ', $token ),
+				str_replace( "\t", ' ', $site ),
+				(int)$eventId,
+				str_replace( "\t", ' ', $deviceMap ),
+				str_replace( "\t", ' ', $countryCode ),
+				str_replace( "\t", ' ', $userAgent ),
+			) );
+			wfErrorLog( $msg, $wgDeviceMapLog );
+		}
+		return $retval;
 	}
 }
